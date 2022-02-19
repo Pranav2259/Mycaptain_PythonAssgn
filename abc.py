9e7852c9e4035121adf0e85374a815fae38b28de
Pranav2259 <85480256+Pranav2259@users.noreply.github.com>
2022-02-20 02:25:10 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,6 +1,6 @@
 # Program to calculate area of a circle
 Pi = 22/7
-radi = float(input('enter Radius of the circle: '))
+radi = float(input('Enter Radius of the circle: '))
 Area = Pi * (radi**2)
 print(Area)
 
